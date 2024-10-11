fd3d82d83e1f97f858bc8115a7e0d3f162270f58
Lilian BELLINI <lilian.bellini@hotmail.fr>
2024-10-11 11:09:15 +0200

    test

---
+++ src/SyliusGoogleMerchantCenterPlugin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Lilian\SyliusGoogleMerchantCenter;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SyliusGoogleMerchantCenterPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
