3fcb199a13b67a357d4bfac8871488165a30101a
Lilian BELLINI <lilian.bellini@hotmail.fr>
2024-10-11 12:47:37 +0200

    correction

---
+++ src/DependencyInjection/SyliusGoogleMerchantCenterExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Lilian\SyliusGoogleMerchantCenter\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class SyliusGoogleMerchantCenterExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
--- src/SyliusGoogleMerchantCenterPlugin.php
+++ src/SyliusGoogleMerchantCenterPlugin.php
@@ -4,8 +4,14 @@ namespace Lilian\SyliusGoogleMerchantCenter;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Lilian\SyliusGoogleMerchantCenter\DependencyInjection\SyliusGoogleMerchantCenterExtension;
 
 class SyliusGoogleMerchantCenterPlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    protected function getContainerExtensionClass(): string
+    {
+        return SyliusGoogleMerchantCenterExtension::class;
+    }
 }
