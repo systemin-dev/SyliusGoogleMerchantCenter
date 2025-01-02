88a4a2561bebea1a43e7722da725f98c9599091d
Nicolas Cestari <nico@systemin.fr>
2025-01-02 14:23:54 +0100

    Fix/Feature: gestion des produits non activés

--- src/Generator/ProductFeedGenerator.php
+++ src/Generator/ProductFeedGenerator.php
@@ -2,6 +2,8 @@
 
 namespace Systemin\SyliusGoogleMerchantCenter\Generator;
 
+
+use Doctrine\Common\Collections\ArrayCollection;
 use Sylius\Component\Core\Repository\ProductRepositoryInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,64 +30,67 @@ class ProductFeedGenerator
         $xml = new \SimpleXMLElement('<rss version="2.0" xmlns:g="http://base.google.com/ns/1.0"></rss>');
         $channel = $xml->addChild('channel');
         $channel->addChild('title', 'Ma Pépinière');
-        $channel->addChild('link', 'https://www.ma-pepiniere.fr');
+        $channel->addChild('link', 'https://ma-pepiniere.fr');
         $channel->addChild('description', 'Découvrez notre collection de plantes et accessoires.');
 
         foreach ($products as $product) {
-            $item = $channel->addChild('item');
-            $item->addChild('g:id', $product->getId(), 'http://base.google.com/ns/1.0');
-            $item->addChild('title', htmlspecialchars($product->getName()), 'http://base.google.com/ns/1.0');
-            $item->addChild('description', htmlspecialchars($product->getDescription()), 'http://base.google.com/ns/1.0');
+            if ($product->isEnabled()) {
+                $item = $channel->addChild('item');
+                $item->addChild('g:id', $product->getId(), 'http://base.google.com/ns/1.0');
+                $item->addChild('title', htmlspecialchars($product->getName()), 'http://base.google.com/ns/1.0');
+                $item->addChild('description', htmlspecialchars($product->getDescription()), 'http://base.google.com/ns/1.0');
 
-            // Génération du lien produit
-            $locale = $product->getTranslation()->getLocale();
-            $link = $this->url . '/' . $locale .  '/produit/' . $product->getSlug();
+                // Génération du lien produit
+                $locale = $product->getTranslation()->getLocale();
+                $link = $this->router->generate('sylius_shop_product_show', [
+                    'slug' => $product->getSlug(),
+                    '_locale' => $locale,
+                ], RouterInterface::ABSOLUTE_URL);
+                $item->addChild('link', $link);
 
-            $item->addChild('link', $link);
+                // Image principale
+                $image = $product->getImages()->first();
+                if ($image) {
+                    $absoluteImageLink = $this->url . '/media/image/' . $image->getPath();
 
-            // Image principale
-            $image = $product->getImages()->first();
-            if ($image) {
-                $absoluteImageLink = $this->url . '/media/image/' . $image->getPath();
-
-                $item->addChild('g:image_link', $absoluteImageLink, 'http://base.google.com/ns/1.0');
-            }
-
-            // Variantes
-            $variant = $product->getVariants()->first();
-
-            if ($variant) {
-                // Disponibilité
-                $availability = StockService::getStockAvailability($product) ? 'in stock' : 'out of stock';
-                $item->addChild('g:availability', $availability, 'http://base.google.com/ns/1.0');
-
-                // Prix
-                $channelPricing = $variant->getChannelPricings()->first();
-                if ($channelPricing !== false) {
-                    $price = $channelPricing->getPrice() / 100; // Diviser par 100 si le prix est en centimes
-                    $item->addChild('g:price', $price . ' EUR', 'http://base.google.com/ns/1.0');
+                    $item->addChild('g:image_link', $absoluteImageLink, 'http://base.google.com/ns/1.0');
                 }
 
-                // Date de disponibilité si précommande
-                if (!$variant->isInStock()) {
-                    $availabilityDate = (new \DateTime())->modify('+1 month')->format('Y-m-d\TH:i:s\Z');
-                    $item->addChild('g:availability_date', $availabilityDate, 'http://base.google.com/ns/1.0');
+                $variant = $product->getVariants()->first();
+
+                if ($variant) {
+                    // Disponibilité
+                    $availability = StockService::getStockAvailability($product) ? 'in stock' : 'out of stock';
+                    $item->addChild('g:availability', $availability, 'http://base.google.com/ns/1.0');
+
+                    // Prix
+                    $channelPricing = $variant->getChannelPricings()->first();
+                    if ($channelPricing !== false) {
+                        $price = $channelPricing->getPrice() / 100; // Diviser par 100 si le prix est en centimes
+                        $item->addChild('g:price', $price . ' EUR', 'http://base.google.com/ns/1.0');
+                    }
+
+                    // Date de disponibilité si précommande
+                    if (!$variant->isInStock()) {
+                        $availabilityDate = (new \DateTime())->modify('+1 month')->format('Y-m-d\TH:i:s\Z');
+                        $item->addChild('g:availability_date', $availabilityDate, 'http://base.google.com/ns/1.0');
+                    }
+
+                    // Identifiant de groupe d'articles
+                    $item->addChild('g:item_group_id', $variant->getCode(), 'http://base.google.com/ns/1.0');
                 }
 
-                // Identifiant de groupe d'articles
-                $item->addChild('g:item_group_id', $variant->getCode(), 'http://base.google.com/ns/1.0');
+                // Marque
+                $item->addChild('g:brand', 'Ma Pépinière', 'http://base.google.com/ns/1.0');
+
+                // Condition
+                $item->addChild('g:condition', 'new', 'http://base.google.com/ns/1.0');
+
+                // Livraison
+                $shipping = $item->addChild('g:shipping', '', 'http://base.google.com/ns/1.0');
+                $shipping->addChild('g:country', 'FR', 'http://base.google.com/ns/1.0');
+                $shipping->addChild('g:price', '14.00 EUR', 'http://base.google.com/ns/1.0'); // Ajustez selon vos règles de livraison
             }
-
-            // Marque
-            $item->addChild('g:brand', 'Ma Pépinière', 'http://base.google.com/ns/1.0');
-
-            // Condition
-            $item->addChild('g:condition', 'new', 'http://base.google.com/ns/1.0');
-
-            // Livraison
-            $shipping = $item->addChild('g:shipping', '', 'http://base.google.com/ns/1.0');
-            $shipping->addChild('g:country', 'FR', 'http://base.google.com/ns/1.0');
-            $shipping->addChild('g:price', '14.00 EUR', 'http://base.google.com/ns/1.0'); // Ajustez selon vos règles de livraison
         }
 
         $response = new Response($xml->asXML());
